274ff62b67c96a4dc75482788d2cbfa665b2abc3
Abhinand Jha <jha.abhinand1@gmail.com>
2025-07-10 21:33:10 -0700

    disable auth check for now since vercel handles that

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
  
+// TODO: Enable once we migrate off of sqlite.
 export async function middleware(request: NextRequest) {
-	const sessionCookie = getSessionCookie(request);
+	// const sessionCookie = getSessionCookie(request);
  
-	if (!sessionCookie) {
-		return NextResponse.redirect(new URL("/", request.url));
-	}
+	// if (!sessionCookie) {
+	// 	return NextResponse.redirect(new URL("/", request.url));
+	// }
  
 	return NextResponse.next();
 }
  
-export const config = {
-	matcher: ["/landing", "/workout", "/workout/:path*"],
-};
+// export const config = {
+// 	matcher: ["/landing", "/workout", "/workout/:path*"],
+// };
